e39151fbe87130678d56222a4006c07d91b4c925
mouni2619 <mounikaadada26@gmail.com>
2024-05-31 20:54:55 +0530

    backend url

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-be-gkzp.onrender.com", {
+			const socket = io("https://chatbackend-7do1.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
